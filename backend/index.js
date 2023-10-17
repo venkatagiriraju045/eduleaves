a6918e4a9cff81096f5fa9060a43c901338f9be7
Venkatagiriraju <110602499+venkatagiriraju045@users.noreply.github.com>
2023-10-17 11:48:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: ["https://eduleaves-frontend.vercel.app"],
+    origin: ["https://edutainment-nine.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
 }));
