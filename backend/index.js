b3d8a37d18047ff0932cb516efe6ab89f22a7191
Venkatagiriraju U <venkatagiriraju045@gmail.com>
2023-11-17 20:34:15 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,12 @@ app.get('/', (req, res) => {
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
 });
 
 const userSchema = new mongoose.Schema({
