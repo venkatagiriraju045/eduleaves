cc7f9547ce955f36cc68f7829c28f1c4f82b8e50
Venkatagiriraju U <venkatagiriraju045@gmail.com>
2023-12-03 10:52:53 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
 const app = express();
 app.use(cors({
@@ -12,7 +12,7 @@ app.use(cors({
 const PORT = 'https://the-students-gate.vercel.app';
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://the-students-gate.vercel.app');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials', 'true');
