4b0ddfeb4fceeab6f998e6a25780d75a04ecbe94
Venkatagiriraju U <venkatagiriraju045@gmail.com>
2024-01-09 13:04:55 +0530

    second commit

--- frontend/src/components/SemResultUpdate.js
+++ frontend/src/components/SemResultUpdate.js
@@ -35,7 +35,7 @@ const SemResultUpdate = () => {
     
                     // Process each row in the Excel sheet
                     excelData.forEach((data) => {
-                        const { registerno, coursecode, coursename, credit, result, grade } = data;
+                        const { registerno, coursecode, coursename, credit, result, grade, semester } = data;
     
                         // Check if register number exists in the resultsToUpdate object
                         if (!semesterResultsToUpdate[registerno]) {
@@ -44,7 +44,7 @@ const SemResultUpdate = () => {
     
                         // Add subject result object to the register number's array
                         semesterResultsToUpdate[registerno].push({
-                            semester:semester,
+                            semester,
                             code:coursecode,
                             name: coursename,
                             credit,
